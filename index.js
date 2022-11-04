f6dc45918c1c011f714d47cef9dbe5269d69f945
xooxaa <37028661+xooxaa@users.noreply.github.com>
2022-11-05 00:18:47 +0100

    Minor refactoring

--- index.js
+++ index.js
@@ -17,25 +17,25 @@ function detectColorScheme() {
 
   //dark theme preferred, set document with a `data-theme` attribute
   if (theme == "dark") {
-    document.documentElement.setAttribute("data-theme", "dark");
+    document.documentElement.setAttribute("dataTheme", "dark");
   }
 }
 detectColorScheme();
 
 //identify the toggle switch HTML element
 const toggleSwitch = document.querySelector(
-  '#theme-switch input[type="checkbox"]'
+  '#themeSwitch input[type="checkbox"]'
 );
 
 //function that changes the theme, and sets a localStorage variable to track the theme between page loads
 function switchTheme(e) {
   if (e.target.checked) {
     localStorage.setItem("theme", "dark");
-    document.documentElement.setAttribute("data-theme", "dark");
+    document.documentElement.setAttribute("dataTheme", "dark");
     toggleSwitch.checked = true;
   } else {
     localStorage.setItem("theme", "light");
-    document.documentElement.setAttribute("data-theme", "light");
+    document.documentElement.setAttribute("dataTheme", "light");
     toggleSwitch.checked = false;
   }
 }
